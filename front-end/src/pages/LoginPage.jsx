2c816911d873a9c15137347239911ab16edc8e86
himank singh <singhhimank241@gmail.com>
2023-08-14 14:48:41 +0530

    patch 1.0.1, added loading spinner at the login page

--- front-end/src/pages/LoginPage.jsx
+++ front-end/src/pages/LoginPage.jsx
@@ -2,12 +2,12 @@ import {ButtonPrimary} from '../components/simple/button'
 import {Link, useNavigate} from 'react-router-dom';
 import { useGoogleLogin } from '@react-oauth/google';
 import { DataContext } from '../context/DataContext';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import setCookie from '../helper/setCookie';
 function LoginPage() {
   const navigate = useNavigate(); 
   const {login, setAppData, setConnectionStatus, setSocketToken} = useContext(DataContext);
-
+  const [loadingStatus, setLoadingStatus] = useState('static')
   const googleLogin = useGoogleLogin({
     onSuccess: async (response) => {
       const {access_token, expires_in} = response;
@@ -29,7 +29,8 @@ function LoginPage() {
           app_data : true,
           socket_token : true,
         }))
-
+        
+        setLoadingStatus('done')
         // navigate to the Home screen
         navigate('/Home')
       })
@@ -40,36 +41,50 @@ function LoginPage() {
           alert(requestMessage)
         }
       })
+
+      // set the state until the app is busy fetching the data
+      setLoadingStatus('pending')
     }
   });
 
   return (
     <>
-      <div className = 'login'>
-        <div className="logo">
-          <span data-color = 'grey'>dev.himank.</span>
-          <span data-color = "blue">todo</span>
-        </div>
-        <p className="note">
-          welcome to dev.himank.todo<br/>
-          login with &nbsp; 
-          <font color = "#4285F4">G</font>
-          <font color = "#EA4335">o</font>
-          <font color = '#FBBC05'>o</font>
-          <font color = "#4285F4">g</font>
-          <font color = '#34A853'>l</font>
-          <font color = "#EA4335">e</font>
-          &nbsp; to continue.
-        </p>
-        <div className="buttons">
-        <ButtonPrimary text = "login" eventHandler={googleLogin}/>
-        </div>
+    <div className = 'login'>
+      <div className="logo">
+        <span data-color = 'grey'>dev.himank.</span>
+        <span data-color = "blue">todo</span>
       </div>
-      <div className="bottom">
-        <Link to = '/Terms'>Terms and condition</Link>
-        <span>|</span>
-        <Link to = '/Privacy'>Privacy Policy</Link>
-      </div>
+      {
+        loadingStatus === 'static' && <><p className="note">
+        welcome to dev.himank.todo<br/>
+        login with &nbsp; 
+        <font color = "#4285F4">G</font>
+        <font color = "#EA4335">o</font>
+        <font color = '#FBBC05'>o</font>
+        <font color = "#4285F4">g</font>
+        <font color = '#34A853'>l</font>
+        <font color = "#EA4335">e</font>
+        &nbsp; to continue.
+      </p>
+      <div className="button">
+      <ButtonPrimary text = "login" eventHandler={googleLogin}/>
+      </div></>
+      }
+      {
+        loadingStatus === 'pending' && <>
+        <p className = 'note'>fetching your data...</p>
+        <p className = 'note note-small'>plz do not reload the web page</p>
+        <div className="spinner-container">
+        <i className = 'fa-solid fa-spinner spinner'/>
+        </div>
+        </>
+      }
+    </div>
+    <div className="bottom">
+      <Link to = '/Terms'>Terms and condition</Link>
+      <span>|</span>
+      <Link to = '/Privacy'>Privacy Policy</Link>
+    </div>
     </>
   )
 }
